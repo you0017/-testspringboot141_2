d60749fdbf07edd1a105639fcd051e9af38daf48
you0017 <2092425375@qq.com>
2024-08-17 13:58:31 +0800

    springboot_junit4_yml_热部署

--- src/main/java/com/yc/controller/HelloController.java
+++ src/main/java/com/yc/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.yc.controller;
 
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Log4j2
 public class HelloController {
 
+    @Autowired
+    private Environment env;
     @GetMapping
     public String hello() {
         //System.out.println("1");
@@ -18,6 +22,12 @@ public class HelloController {
         log.debug("debug");
         log.info("info");
         log.warn("warning");
+
+
+        log.info(env.getProperty("JAVA_HOME"));
+        log.info(env.getProperty("product.pname") + "\t" + env.getProperty("myfilepath"));
+        log.info(env.getProperty("server.port"));
+
         return "hello world";
     }
 }
